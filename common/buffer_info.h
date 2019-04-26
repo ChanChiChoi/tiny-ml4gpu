3d58c0fe71ae7e9b4d264ad7f5a920e09c844add
shouhuxianjian <342888106@qq.com>
2019-04-26 18:52:17 +0800

    add Buf

--- common/buffer_info.h
+++ common/buffer_info.h
@@ -11,7 +11,7 @@
 
 
 /// Information record describing a Python buffer object
-struct buffer_info {
+typedef struct buffer_info {
     void *ptr = nullptr;          // Pointer to the underlying storage
     bool isdevice = false;        // whether the buffer on device
     ssize_t itemsize = 0;         // Size of individual items in bytes
@@ -21,6 +21,6 @@ struct buffer_info {
     std::vector<ssize_t> shape;   // Shape of the tensor (1 entry per dimension)
     std::vector<ssize_t> strides; // Number of entries between adjacent entries (for each per dimension)
 
-};
+} Buf;
 
 
