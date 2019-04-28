2b657f59302661c9631ae1c1f9c2ed6ee75ccaf9
shouhuxianjian <342888106@qq.com>
2019-04-28 18:00:35 +0800

    add some construction

--- common/buffer_info.h
+++ common/buffer_info.h
@@ -33,6 +33,41 @@ typedef struct buffer_info {
     std::vector<ssize_t> shape;   // Shape of the tensor (1 entry per dimension)
     std::vector<ssize_t> strides; // Number of entries between adjacent entries (for each per dimension)
 
+    buffer_info() {}
+
+    buffer_info(void *ptr_host, void *ptr_device, ssize_t itemsize, Dtype dtype, ssize_t ndim,
+                std::vector<ssize_t> shape, std::vector<ssize_t> strides)
+    :ptr_host(ptr_host), ptr_device(ptr_device), itemsize(itemsize), dtype(dtype), ndim(ndim), 
+     shape(std::move(shape)), strides(std::move(strides)){
+        if (ndim != (ssize_t) shape.size() || ndim != (ssize_t) strides.size())
+            assert("buffer_info: ndim do not match shape and/or strides length");
+        for (size_t i = 0; i<(size_t)ndim; i++)
+            size *= shape[i];
+    }
+
+//    template<typename T>
+//    buffer_info(T *ptr_host, T *ptr_device, std::vector<ssize_t>shape, std::vector<ssize_t>strides)
+//    :buffer_info(ptr_host, ptr_device, sizeof(T),)
+
+    buffer_info(const buffer_info &) = delete;
+    buffer_info& operator=(const buffer_info &) = delete;
+   
+    buffer_info(buffer_info &&other){
+        (*this) = std::move(other);
+    }
+
+    buffer_info& operator=(buffer_info &&rhs){
+        ptr_host = rhs.ptr_host;
+        ptr_device = rhs.ptr_device;
+        itemsize = rhs.itemsize;
+        size = rhs.size;
+        ndim = rhs.ndim;
+        shape = std::move(rhs.shape);
+        strides = std::move(rhs.strides);
+        return *this;
+    }
+
+
 } Buf;
 
 
