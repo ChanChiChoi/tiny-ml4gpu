f5280f43fa9f261d0c9d2ae0a8fb391722e03794
shouhuxianjian <342888106@qq.com>
2019-04-29 14:39:06 +0800

    add some feature

--- common/buffer_info.h
+++ common/buffer_info.h
@@ -11,45 +11,48 @@
 #include "common/common.h"
 
 
-/// Information record describing a Python buffer object
 typedef struct buffer_info {
+
     void *ptr_host = nullptr;          // Pointer to the underlying storage on host
     void *ptr_device = nullptr;        // Pointer to the underlying storage on device
-    ssize_t itemsize = 0;         // Size of individual items in bytes
-    ssize_t size = 0;             // Total number of entries
+    size_t itemsize = 0;         // Size of individual items in bytes
+    size_t size = 1;             // Total number of entries
     Dtype dtype;         // record the type of item in buffer;
     std::string format;           // For compatible with pybind11 buffer_info, 
 
-    /*
- *  ndim: 
- *    0 - scalar:  x 
- *    1 - vector[cols], [1, cols]:  [x],[x,x,x]
- *    2 - matrix[rows, cols]:   [[x,x],[x,x]]
- *
- *    ...
- *
- * */
-    ssize_t ndim = 0;             // Number of dimensions
-    std::vector<ssize_t> shape;   // Shape of the tensor (1 entry per dimension)
-    std::vector<ssize_t> strides; // Number of entries between adjacent entries (for each per dimension)
+   /*
+    *  ndim: 
+    *    0 - scalar:  x 
+    *    1 - vector[cols], [1, cols]:  [x],[x,x,x]
+    *    2 - matrix[rows, cols]:   [[x,x],[x,x]]
+    *
+    *    ...
+    *
+    * */
+    size_t ndim = 0;             // Number of dimensions
+    std::vector<size_t> shape;   // Shape of the tensor (1 entry per dimension): must be 4 dim,[1,2,0,0]
+    std::vector<size_t> strides; // Number of entries between adjacent entries (for each per dimension)
 
-    buffer_info() {}
+    buffer_info() {
+        for (size_t i = 0; i< ndim; i++)
+            size *= shape[i];
+    }
 
-    buffer_info(void *ptr_host, void *ptr_device, ssize_t itemsize, Dtype dtype, ssize_t ndim,
-                std::vector<ssize_t> shape, std::vector<ssize_t> strides)
-    :ptr_host(ptr_host), ptr_device(ptr_device), itemsize(itemsize), dtype(dtype), ndim(ndim), 
-     shape(std::move(shape)), strides(std::move(strides)){
-        if (ndim != (ssize_t) shape.size() || ndim != (ssize_t) strides.size())
+    buffer_info(void * ptr_host, unsigned long itemsize, Dtype dtype, int ndim,
+                any_container<size_t> shape_in , any_container<size_t> strides_in )
+    :ptr_host(ptr_host), itemsize(itemsize), dtype(dtype), ndim(ndim), 
+     shape(std::move(shape_in) ), strides(std::move(strides_in)){
+        ptr_device = NULL;
+        if (ndim != (size_t) shape.size() || ndim != (size_t) strides.size())
             assert("buffer_info: ndim do not match shape and/or strides length");
+        size = 1; // in case of result of *= is 0;
         for (size_t i = 0; i<(size_t)ndim; i++)
             size *= shape[i];
     }
 
-//    template<typename T>
-//    buffer_info(T *ptr_host, T *ptr_device, std::vector<ssize_t>shape, std::vector<ssize_t>strides)
-//    :buffer_info(ptr_host, ptr_device, sizeof(T),)
 
     buffer_info(const buffer_info &) = delete;
+
     buffer_info& operator=(const buffer_info &) = delete;
    
     buffer_info(buffer_info &&other){
@@ -66,7 +69,6 @@ typedef struct buffer_info {
         strides = std::move(rhs.strides);
         return *this;
     }
-
 
 } Buf;
 
