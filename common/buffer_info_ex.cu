e9fdaebc1cb019fa72c1b878d9171824bb6c720d
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-04-30 19:29:46 +0800

    Update buffer_info_ex.cu

--- common/buffer_info_ex.cu
+++ common/buffer_info_ex.cu
@@ -12,3 +12,24 @@ Array & Array::cuda(){
     return *this;
 
 }
+
+class Array{
+
+    Buf *ptr_buf;
+
+    Array() {}
+
+    Array(py::array_t<float> &array){
+        auto array_info = array.requests();
+
+        if (array_info.format != py::format_descriptor<float>::format())
+            throw std::runtime_error("Incompatible format: excepted a float32 array!");
+        if (array_info.ndim != 2)
+            throw std::runtime_error("Incompatible buffer dimension!");
+
+        this->ptr_buf = new Buf(array_info);
+    }
+
+    Array & cuda();
+
+};
