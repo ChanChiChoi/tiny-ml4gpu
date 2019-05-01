148c5df103e3310bcc54d369296b33907fccad01
ChanChiChoi <342888106@qq.com>
2019-05-01 12:48:55 +0000

    change name

---
+++ common/helper.cuh
@@ -0,0 +1,23 @@
+
+# ifndef __HELPER__
+# define __HELPER__
+#include<stdio.h>
+
+static void HandleError( cudaError_t err,
+                         const char *file,
+                         int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
+#define CHECK_CALL( err ) (HandleError( err, __FILE__, __LINE__ ))
+
+
+#define HANDLE_NULL( a ) {if (a == NULL) { \
+                            printf( "Host memory failed in %s at line %d\n", \
+                                    __FILE__, __LINE__ ); \
+                            exit( EXIT_FAILURE );}}
+
+# endif
