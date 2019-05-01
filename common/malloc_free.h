47b94ecc41970b91c250fb484f681c4c7a4a41ab
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-01 17:25:43 +0800

    Update malloc_free.h

--- common/malloc_free.h
+++ common/malloc_free.h
@@ -3,11 +3,11 @@
 template<class T> T *
 device_malloc(size_t size);
 
-template<typename T> T *
-host_to_device(T * ptr_host, size_t size);
+float *
+host_to_device(float * ptr_host, size_t size);
 
 template<typename T> T *
 device_free(T *ptr_device);
 
-template<typename T> T *
-device_to_host(T * ptr_device, T *ptr_host, size_t size);
+float *
+device_to_host(float * ptr_device, float *ptr_host, size_t size);
