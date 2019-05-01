25086af463823ef9f49474f75267acef101e6df8
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-01 16:08:21 +0800

    Update buffer_info_ex.cpp

--- common/buffer_info_ex.cpp
+++ common/buffer_info_ex.cpp
@@ -6,7 +6,7 @@ Buf& buffer_info_ex::cuda(){
     size_t bytes_size = itemsize * size;
     switch (format[0]){
         case 'f':
-            this -> ptr_device = host_to_device(ptr, bytes_size);
+            ptr_device = (void *)host_to_device((float *)ptr, bytes_size);
             break;
         default:
             throw std::runtime_error("current version only support float32!");
@@ -17,7 +17,7 @@ Buf& buffer_info_ex::cuda(){
 }
 
 Array & Array::cuda(){
-    this->cuda();
+    ptr_buf->cuda();
     return *this;
 
 }
