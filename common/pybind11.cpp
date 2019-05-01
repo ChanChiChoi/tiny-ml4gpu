424b229a78fa4250170649de6b861cee22966d00
ChanChiChoi <342888106@qq.com>
2019-05-01 13:56:19 +0000

    add makefile

---
+++ common/pybind11.cpp
@@ -0,0 +1,16 @@
+#include<pybind11/pybind11.h>
+#include<pybind11/numpy.h>
+#include "common/buffer_info_ex.h"
+
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(Array, m){
+
+    py::class_<Array>(m,"Array")
+        .def(py::init<py::array_t<float> &>())
+        .def("cuda", &Array::cuda)
+        ;
+
+
+}
