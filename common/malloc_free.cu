9992fb3bd2d65334e446ea5602d10fd4a407f240
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-01 17:25:09 +0800

    Update malloc_free.cu

--- common/malloc_free.cu
+++ common/malloc_free.cu
@@ -1,7 +1,10 @@
 #include <string>
 #include "common/malloc_free.h"
 #include "common/helper.cuh"
-
+/*
+ because nvcc will not Instantiate template onto host side of shared object file,
+so, other host shared object file by g++  can not find function symbols in shared object by nvcc
+*/
 
 template<class T> T *
 device_malloc(size_t size){
@@ -12,10 +15,10 @@ device_malloc(size_t size){
 }
 
 
-template<typename T> T *
-host_to_device(T * ptr_host, size_t size){
+float *
+host_to_device(float * ptr_host, size_t size){
 
-    T *ptr_device = device_malloc<T>(size);
+    float *ptr_device = device_malloc<float>(size);
     // copy host data to device;
     CHECK_CALL(cudaMemcpy(ptr_device, ptr_host, size, cudaMemcpyHostToDevice));
     return ptr_device;
@@ -30,11 +33,11 @@ device_free(T *ptr_device){
 }
 
 
-template<typename T> T *
-device_to_host(T * ptr_device, T *ptr_host, size_t size){
+float *
+device_to_host(float * ptr_device, float *ptr_host, size_t size){
 
     CHECK_CALL(cudaMemcpy(ptr_host, ptr_device, size, cudaMemcpyDeviceToHost));
-    device_free<T>(ptr_device);
+    device_free<float>(ptr_device);
     ptr_device = NULL;
     return ptr_device;
 }
