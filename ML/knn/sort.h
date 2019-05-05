962c9933f737cc11e42c2f03f327b03cc1935400
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-05 16:18:01 +0800

    Update sort.h

--- ML/knn/sort.h
+++ ML/knn/sort.h
@@ -1,3 +1,8 @@
+typedef unsigned int u32;
 
 void
-sort_by_rows_cpu(unsigned int *mat,unsigned int *ind_mat, size_t rows, size_t cols);
+sort_by_rows(float *mat, u32 *ind_mat, size_t rows, size_t cols, u32 precision);
+
+
+void
+sort_by_rows(u32 *mat, u32 *ind_mat, size_t rows, size_t cols, u32 precision);
