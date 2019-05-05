136d60184ce84e098b78fcc577e0648e87f10cbf
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-05 11:43:44 +0800

    Update buffer_info_ex.cpp

--- common/buffer_info_ex.cpp
+++ common/buffer_info_ex.cpp
@@ -6,7 +6,7 @@ Buf& buffer_info_ex::cuda(){
     size_t bytes_size = itemsize * size;
     switch (format[0]){
         case 'f':
-            ptr_device = (void *)host_to_device((float *)ptr, bytes_size);
+            ptr_device = (void *)host_to_device_malloc((float *)ptr, bytes_size);
             break;
         default:
             throw std::runtime_error("current version only support float32!");
