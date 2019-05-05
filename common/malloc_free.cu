8e86549ed27fe2ea8f1ca46e3a47301e0a332ce7
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-05 11:40:06 +0800

    Update malloc_free.cu

--- common/malloc_free.cu
+++ common/malloc_free.cu
@@ -6,6 +6,7 @@
 so, other host shared object file by g++  can not find function symbols in shared object by nvcc
 */
 
+//==============malloc template
 template<class T> T *
 device_malloc(size_t size){
 
@@ -14,25 +15,45 @@ device_malloc(size_t size){
     return ptr_device;
 }
 
+template<typename T> T *
+_host_to_device_malloc(T *ptr_host, size_t size){
 
-float *
-host_to_device(float * ptr_host, size_t size){
-
-    float *ptr_device = device_malloc<float>(size);
+    T *ptr_device = device_malloc<T>(size);
     // copy host data to device;
     CHECK_CALL(cudaMemcpy(ptr_device, ptr_host, size, cudaMemcpyHostToDevice));
     return ptr_device;
+}
+
+template<typename T> T *
+_host_to_device(T *ptr_device, T *ptr_host, size_t size){
+
+    // copy host data to device;
+    CHECK_CALL(cudaMemcpy(ptr_device, ptr_host, size, cudaMemcpyHostToDevice));
+    return ptr_device;
+}
+
+//===========template Instantiation
+float *
+host_to_device_malloc(float *ptr_host, size_t size){
+    return _host_to_device_malloc<float>(ptr_host, size);
 }
 
 unsigned int *
-host_to_device(unsigned int * ptr_host, size_t size){
-
-    unsigned int *ptr_device = device_malloc<unsigned int>(size);
-    // copy host data to device;
-    CHECK_CALL(cudaMemcpy(ptr_device, ptr_host, size, cudaMemcpyHostToDevice));
-    return ptr_device;
+host_to_device_malloc(unsigned int * ptr_host, size_t size){
+    return _host_to_device_malloc<unsigned int>(ptr_host, size);
 }
 
+float *
+host_to_device(float *ptr_device, float *ptr_host, size_t size){
+    return _host_to_device<float>(ptr_device, ptr_host, size);
+}
+
+unsigned int *
+host_to_device(unsigned int *ptr_device, unsigned int *ptr_host, size_t size){
+    return _host_to_device<unsigned int>(ptr_device, ptr_host, size);
+}
+
+//==================free template
 template<typename T> T *
 device_free(T *ptr_device){
 
@@ -41,21 +62,40 @@ device_free(T *ptr_device){
 }
 
 
-float *
-device_to_host(float * ptr_device, float *ptr_host, size_t size){
+template <typename T> T*
+_device_to_host_free(T * ptr_host, T *ptr_device, size_t size){
 
     CHECK_CALL(cudaMemcpy(ptr_host, ptr_device, size, cudaMemcpyDeviceToHost));
-    device_free<float>(ptr_device);
+    device_free<T>(ptr_device);
     ptr_device = NULL;
     return ptr_device;
 }
 
-unsigned int *
-device_to_host(unsigned int * ptr_device, unsigned int *ptr_host, size_t size){
+template <typename T> T*
+_device_to_host(T * ptr_host, T *ptr_device, size_t size){
 
     CHECK_CALL(cudaMemcpy(ptr_host, ptr_device, size, cudaMemcpyDeviceToHost));
-    device_free<unsigned int>(ptr_device);
-    ptr_device = NULL;
     return ptr_device;
+}
+
+//=============template Instantiation
+float *
+device_to_host_free(float * ptr_host, float *ptr_device, size_t size){
+    return _device_to_host_free<float>(ptr_host, ptr_device, size);
+}
+
+unsigned int *
+device_to_host_free(unsigned int * ptr_host, unsigned int  *ptr_device, size_t size){
+    return _device_to_host_free<unsigned int>(ptr_host, ptr_device, size);
+}
+
+float *
+device_to_host(float * ptr_host, float *ptr_device, size_t size){
+    return _device_to_host<float>(ptr_host, ptr_device, size);
+}
+
+unsigned int *
+device_to_host(unsigned int * ptr_host, unsigned int  *ptr_device, size_t size){
+    return _device_to_host<unsigned int>(ptr_host, ptr_device, size);
 }
 
