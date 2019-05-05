bcd29597360a72902bc1f205ff95ada672d7d603
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-05 11:42:48 +0800

    Update malloc_free.h

--- common/malloc_free.h
+++ common/malloc_free.h
@@ -1,7 +1,14 @@
 #pragma once
 
+//=========malloc
 template<class T> T *
 device_malloc(size_t size);
+
+float *
+host_to_device_malloc(float * ptr_host, size_t size);
+
+unsigned int *
+host_to_device_malloc(unsigned int * ptr_host, size_t size);
 
 float *
 host_to_device(float * ptr_host, size_t size);
@@ -9,8 +16,15 @@ host_to_device(float * ptr_host, size_t size);
 unsigned int *
 host_to_device(unsigned int * ptr_host, size_t size);
 
+//===========free
 template<typename T> T *
 device_free(T *ptr_device);
+
+float *
+device_to_host_free(float * ptr_device, float *ptr_host, size_t size);
+
+unsigned int *
+device_to_host_free(unsigned int * ptr_device, unsigned int *ptr_host, size_t size);
 
 float *
 device_to_host(float * ptr_device, float *ptr_host, size_t size);
