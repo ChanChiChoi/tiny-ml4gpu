dcc0a43f14aa6c9f7afeb58960f35d7380345078
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-08 16:55:02 +0800

    Update math.h

--- ML/math/math.h
+++ ML/math/math.h
@@ -5,3 +5,7 @@ void
 matrix_mul_cpu(float *Md, u32 Row_Md, u32 Col_Md,
                float *Nd, u32 Row_Nd, u32 Col_Nd,
                float *Pd, u32 Row_Pd, u32 Col_Pd);
+
+void
+cov_cpu(float *mat, u32 Row_mat, u32 Col_mat,
+        float *mat_cov, u32 Row_mat_cov, u32 Col_mat_cov);
