5a939632a9901bc9615ff1fe871f1d507b407fa2
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-10 11:18:55 +0800

    Update helper.cuh

--- common/helper.cuh
+++ common/helper.cuh
@@ -1,23 +1,7 @@
+#pragma once
 
-# ifndef __HELPER__
-# define __HELPER__
-#include<stdio.h>
+#include "NVIDIA/common/inc/helper_cuda.h"
 
-static void HandleError( cudaError_t err,
-                         const char *file,
-                         int line ) {
-    if (err != cudaSuccess) {
-        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
-                file, line );
-        exit( EXIT_FAILURE );
-    }
-}
-#define CHECK_CALL( err ) (HandleError( err, __FILE__, __LINE__ ))
+#define CHECK_CALL( err, file, line ) check((err), #err, file, line)
 
-
-#define HANDLE_NULL( a ) {if (a == NULL) { \
-                            printf( "Host memory failed in %s at line %d\n", \
-                                    __FILE__, __LINE__ ); \
-                            exit( EXIT_FAILURE );}}
-
-# endif
+#define LAST_ERROR(msg, file, line)  __getLastCudaError(msg, file, line)
