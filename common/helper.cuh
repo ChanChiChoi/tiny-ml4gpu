6f6197090bf5ef3248fd1cdcd59131a549ec3a6a
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-10 11:59:55 +0800

    Update helper.cuh

--- common/helper.cuh
+++ common/helper.cuh
@@ -3,5 +3,6 @@
 #include "NVIDIA/common/inc/helper_cuda.h"
 
 #define CHECK_CALL( err, file, line ) check((err), #err, file, line)
+#define CHECK_CALL_DEFAULT(err) check((err), #err, __FILE, __LINE__)
 
 #define LAST_ERROR(msg, file, line)  __getLastCudaError(msg, file, line)
