c6797e0ea0f4a8be43738f846de83233b8c409bb
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-10 11:40:49 +0800

    Update pca.cpp

--- ML/decomposition/pca.cpp
+++ ML/decomposition/pca.cpp
@@ -30,7 +30,7 @@ PCA::fit(Array &matrix){
     size_t size_mean = sizeof(float)*cols;
 
     float *mean = (float *)malloc(size_mean);
-    float *mean_device = host_to_device_malloc(mean, size_mean);
+    float *mean_device = HOST_TO_DEVICE_MALLOC(mean, size_mean);
 
     mean_by_rows_cpu(ptr_device, mean_device, rows, cols);
 
@@ -38,7 +38,7 @@ PCA::fit(Array &matrix){
     size_t cols_cov = cols;
     size_t size_cov = sizeof(float)*rows_cov*cols_cov;
     float *mat_cov = (float *)malloc(size_cov);
-    float *mat_cov_device = host_to_device_malloc(mat_cov, size_cov);
+    float *mat_cov_device = HOST_TO_DEVICE_MALLOC(mat_cov, size_cov);
     //2 - calc the cov matrix
     cov_cpu(ptr_device, rows, cols,
             mat_cov_device, rows_cov, cols_cov );
@@ -47,7 +47,7 @@ PCA::fit(Array &matrix){
     //5 - preserve the first nth eigval
     //6 - use eigvec to transfer the origin matrix into new space.
 
-    device_to_host_free(mean, mean_device, size_mean);
+    DEVICE_TO_HOST_FREE(mean, mean_device, size_mean);
     free(mean);
 }
 
