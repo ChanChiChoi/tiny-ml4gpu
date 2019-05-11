b94f7b33c8edd8f4c97553f67c59db7d2bff18bb
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-11 12:09:41 +0800

    Update svd.cu

--- ML/math/svd.cu
+++ ML/math/svd.cu
@@ -9,45 +9,37 @@
 #include "common/malloc_free.h"
 #include "common/helper.cuh"
 
-/* cusolverDnDgesvd only support double* pointer. so _svd() has to been double parameter
-*/
-template<typename T> void
-_svd(T *A_device, const int Row_A, const int Col_A, const int lda,
-    T *U_device, const int Row_U, const int Col_U,
-    T *S_device, const int Length,
-    T *VT_device, const int Row_VT, const int Col_VT){
+
+void
+svd(float *A_device, const int Row_A, const int Col_A, const int lda,
+    float *U_device, const int Row_U, const int Col_U,
+    float *S_device, const int Length,
+    float *VT_device, const int Row_VT, const int Col_VT){
 
   cusolverDnHandle_t cusolverH = NULL;
   cublasHandle_t cublasH = NULL;
-
-  T *rwork_device = NULL;
+  float *rwork_device = NULL;
 
   int lwork = 0;
   int info_gpu = 0;
 
-  //step1:
   CHECK_CALL_DEFAULT(cusolverDnCreate(&cusolverH));
   CHECK_CALL_DEFAULT(cublasCreate(&cublasH));
 
-  //step2:
-//  T *A_device = HOST_TO_DEVICE_MALLOC(A, sizeof(T)*lda*Col_A)；
-//  T *S_device = DEVICE_MALLOC(sizeof(T)*Col_A)；
-//  T *U_device = DEVICE_MALLOC(sizeof(T)*lda*Row_A)；
-//  T *VT_device = DEVICE_MALLOC(sizeof(T)*lda*Col_A)；
-  
   int *devInfo_device;
   devInfo_device = DEVICE_MALLOC(devInfo_device,sizeof(int));
 
-  //step3
-  CHECK_CALL_DEFAULT(cusolverDnDgesvd_bufferSize(cusolverH,Row_A,Col_A,&lwork));
+  //cusolverDnSgesvd_bufferSize single precision
+  CHECK_CALL_DEFAULT(cusolverDnSgesvd_bufferSize(cusolverH,Row_A,Col_A,&lwork));
 
-  T *work_device;
-  work_device = DEVICE_MALLOC(work_device,sizeof(T)*lwork);
+  float *work_device;
+  work_device = DEVICE_MALLOC(work_device,sizeof(float)*lwork);
 
   //step 4:compute svd
   signed char jobu = 'A';
   signed char jobvt = 'A';
-  CHECK_CALL_DEFAULT(cusolverDnDgesvd(cusolverH, jobu, jobvt, 
+  //cusolverDnSgesvd single precision
+  CHECK_CALL_DEFAULT(cusolverDnSgesvd(cusolverH, jobu, jobvt, 
                           Row_A, Col_A,
                           A_device, lda,
                           S_device, U_device,lda, 
@@ -66,31 +58,50 @@ _svd(T *A_device, const int Row_A, const int Col_A, const int lda,
   return ;
 }
 
-/*
 void
-svd(float *A, const int Row_A, const int Col_A, const int lda,
-    float *U, const int Row_U, const int Col_U,
-    float *S, const int Length,
-    float *VT, const int Row_VT, const int Col_VT){
+svd(double *A_device, const int Row_A, const int Col_A, const int lda,
+    double *U_device, const int Row_U, const int Col_U,
+    double *S_device, const int Length,
+    double *VT_device, const int Row_VT, const int Col_VT){
 
-    _svd<float>(A, Row_A, Col_A, lda,
-        U, Row_U, Col_U,
-        S, Length,
-        VT, Row_VT, Col_VT);
+  cusolverDnHandle_t cusolverH = NULL;
+  cublasHandle_t cublasH = NULL;
+  double *rwork_device = NULL;
+
+  int lwork = 0;
+  int info_gpu = 0;
+
+  CHECK_CALL_DEFAULT(cusolverDnCreate(&cusolverH));
+  CHECK_CALL_DEFAULT(cublasCreate(&cublasH));
+
+  int *devInfo_device;
+  devInfo_device = DEVICE_MALLOC(devInfo_device,sizeof(int));
+
+  //cusolverDnDgesvd_bufferSize double precision
+  CHECK_CALL_DEFAULT(cusolverDnDgesvd_bufferSize(cusolverH,Row_A,Col_A,&lwork));
+
+  double *work_device;
+  work_device = DEVICE_MALLOC(work_device,sizeof(double)*lwork);
+
+  //step 4:compute svd
+  signed char jobu = 'A';
+  signed char jobvt = 'A';
+  //cusolverDnDgesvd double precision
+  CHECK_CALL_DEFAULT(cusolverDnDgesvd(cusolverH, jobu, jobvt, 
+                          Row_A, Col_A,
+                          A_device, lda,
+                          S_device, U_device,lda, 
+                          VT_device, lda, work_device, lwork, rwork_device, devInfo_device));
+
+  CHECK_CALL_DEFAULT(cudaDeviceSynchronize());
+ 
+  DEVICE_TO_HOST_FREE(&info_gpu, devInfo_device, sizeof(int));  
+
+  DEVICE_FREE(work_device);
+  DEVICE_FREE(rwork_device);
+
+  cublasDestroy(cublasH);
+  cusolverDnDestroy(cusolverH);
+    
+  return ;
 }
-*/
-
-void
-svd(double *A, const int Row_A, const int Col_A, const int lda,
-    double *U, const int Row_U, const int Col_U,
-    double *S, const int Length,
-    double *VT, const int Row_VT, const int Col_VT){
-
-    _svd<double>(A, Row_A, Col_A, lda,
-        U, Row_U, Col_U,
-        S, Length,
-        VT, Row_VT, Col_VT);
-}
-
-
-
