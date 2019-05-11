36ed99694f0fd8dcfc442164ca5015adfcf9e749
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-11 12:29:00 +0800

    Update data.cu

--- ML/preprocessing/data.cu
+++ ML/preprocessing/data.cu
@@ -1,8 +1,6 @@
 #include <cmath>
 #include <stdio.h>
 #include <cuda_runtime.h>
-#include <chrono>
-#include "common/malloc_free.h"
 #include "common/type.h"
 #include "common/common.h"
 
@@ -22,7 +20,7 @@ mean_by_rows(T *mat_device, T *mean_vec, u32 rows, u32 cols){
         for (u32 i = 0; i < rows; i++){
             cur_val = mat_device[i*cols+thread_idx];
             // in case of sum is too big
-            mean = mean*((float)i/(i+1)) +  cur_val/(i+1);
+            mean = mean*((float)i/(i+1)) +  cur_val/(double)(i+1);
         }
 
         mean_vec[thread_idx] = mean;
@@ -46,11 +44,11 @@ zero_mean_by_rows(T *mat_device, T *mean_vec, u32 rows, u32 cols){
 
 //export the function for be called by host
 void
-mean_by_rows_cpu(float *mat_device, float *mean_device, u32 rows, u32 cols){
+mean_by_rows_launch(float *mat_device, float *mean_device, u32 rows, u32 cols){
 
 
     const u32 COLS = 256;
-    dim3 grid0( MAX(1,ceil(cols/COLS)) );
+    dim3 grid0( MAX(1,ceil((double)cols/COLS)) );
     dim3 block0(COLS);
 
     mean_by_rows<float><<<grid0, block0>>>(mat_device, mean_device, rows, cols);
@@ -58,8 +56,8 @@ mean_by_rows_cpu(float *mat_device, float *mean_device, u32 rows, u32 cols){
     const u32 block_size = 32;
     dim3 block1(block_size, block_size);
 
-    dim3 grid1(MAX(1, ceil(cols/block_size)),
-              MAX(1, ceil(rows/block_size)));
+    dim3 grid1(MAX(1, ceil((double)cols/block_size)),
+              MAX(1, ceil((double)rows/block_size)));
 
     zero_mean_by_rows<float><<<grid1,block1>>>(mat_device, mean_device, rows, cols);
 }
