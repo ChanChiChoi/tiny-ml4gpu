6ccfd13570e8d28d3d9188fc9f25d2eeefcd73b7
ChanChiChoi <ChanChiChoi@users.noreply.github.com>
2019-05-12 11:57:42 +0800

    Update buffer_info_ex.cpp

--- common/buffer_info_ex.cpp
+++ common/buffer_info_ex.cpp
@@ -16,6 +16,21 @@ Buf& buffer_info_ex::cuda(){
     return *this;
 }
 
+buffer_info_ex::~buffer_info_ex(){
+
+    switch (format[0]){
+        case 'f':
+            DEVICE_FREE((float *)ptr_device);
+            ptr_device = NULL;
+            break;
+        default:
+            throw std::runtime_error("current version only support float32!");
+            break;
+    }
+
+}
+
+
 Array & Array::cuda(){
     ptr_buf->cuda();
     return *this;
