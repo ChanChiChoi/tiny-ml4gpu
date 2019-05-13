cf52f259c0fda87702d85a2b5f9b48aa63a552f3
shouhuxianjian <342888106@qq.com>
2019-05-13 14:52:58 +0800

    add comment

--- common/pybind11.cpp
+++ common/pybind11.cpp
@@ -1,3 +1,7 @@
+/*
+ 
+g++  pybind11.cpp buffer_info_ex.cpp -shared -std=c++11 -fPIC -I../pybind11/include  -I/root/miniconda3/include/python3.6m -o Array`python3-config --extension-suffix` -I../ -L. -lmalloc_free
+ * */
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include "common/buffer_info_ex.h"
