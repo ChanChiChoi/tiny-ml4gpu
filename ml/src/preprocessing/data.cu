47732685f49b220f4af22579d8065bed0e6feb7e
shouhuxianjian <342888106@qq.com>
2019-05-20 16:45:08 +0800

    remove print

--- ml/src/preprocessing/data.cu
+++ ml/src/preprocessing/data.cu
@@ -14,24 +14,18 @@ mean_by_rows(T *mat_device, T *mean_vec, u32 rows, u32 cols){
 
     u32 thread_idx = idy*(gridDim.x*blockIdx.x) + idx;
 
-    if(thread_idx < cols){
-        printf("[outside] cur thread:%d cols %d\n", thread_idx, cols);
-        T mean = (T)0;
-        T cur_val = (T)0;
-        for (u32 i = 0; i < rows; i++){
-            cur_val = mat_device[i*cols+thread_idx];
-//            // in case of sum is too big
-            mean = mean*((float)i/(i+1)) +  cur_val/(double)(i+1);
-        }
-//    
-        mean_vec[thread_idx] = mean;
-        printf("mean: %f\n",mean);
-    }
-    else
-    {
-        printf("[inside] cur thread:%d cols %d\n", thread_idx, cols);
+    if(thread_idx >= cols)
+        return ;
 
+    T mean = (T)0;
+    T cur_val = (T)0;
+    for (u32 i = 0; i < rows; i++){
+        cur_val = mat_device[i*cols+thread_idx];
+        // in case of sum is too big
+        mean = mean*((float)i/(i+1)) +  cur_val/(double)(i+1);
     }
+    mean_vec[thread_idx] = mean;
+    
 }
 
 // each row subtract the mean vector
@@ -41,14 +35,9 @@ zero_mean_by_rows(T *mat_device, T *mean_vec, u32 rows, u32 cols){
     u32 idy = blockIdx.y*gridDim.y + threadIdx.y;
     u32 idx = blockIdx.x*gridDim.x + threadIdx.x;
 
-
-
     if(idx < cols && idy < rows){
-        printf("zero %d %d\n",idx, idy);
         u32 val_idx = idy*cols + idx;
         mat_device[val_idx] -= mean_vec[idx];
-        if(idx == 0)
-            printf("subtract mean:%f\n",mat_device[val_idx]);
     }
 
 }
@@ -112,7 +101,6 @@ mean_by_rows_launch(float *mat_device, float *mean_device, u32 rows, u32 cols){
     dim3 grid1(MAX(1, ceil((double)cols/block_size)),
               MAX(1, ceil((double)rows/block_size)));
 
-    printf("here:\n");
     zero_mean_by_rows<float><<<grid1,block1>>>(mat_device, mean_device, rows, cols);
 }
 
