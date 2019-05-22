cef65e4114e79fe9f16c0562d0627d8016a30914
shouhuxianjian <342888106@qq.com>
2019-05-22 10:52:41 +0800

    update

--- ml/include/math/vector.h
+++ ml/include/math/vector.h
@@ -2,4 +2,4 @@
 #include "common/include/type.h"
 void
 vector_repeat_by_rows_cpu(float *mat_device, u32 rows_mat, u32 cols_mat,
-                          float *vector_device, cols_vec)
+                          float *vector_device, u32 cols_vec);
