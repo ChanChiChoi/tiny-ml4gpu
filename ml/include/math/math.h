585d1658119b4200090bda8ff7b32afe8cff4612
shouhuxianjian <342888106@qq.com>
2019-05-22 10:50:56 +0800

    add two head file

--- ml/include/math/math.h
+++ ml/include/math/math.h
@@ -3,3 +3,4 @@
 #include "ml/include/math/matrix.h"
 #include "ml/include/math/svd.h"
 #include "ml/include/math/stats.h"
+#include "ml/include/math/vector.h"
---
+++ ml/include/math/vector.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "common/include/type.h"
+void
+vector_repeat_by_rows_cpu(float *mat_device, u32 rows_mat, u32 cols_mat,
+                          float *vector_device, cols_vec)
