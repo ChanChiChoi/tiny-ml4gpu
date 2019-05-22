6bf1d9afa074ef246bf5d031ab72a663ae3ebc6b
shouhuxianjian <342888106@qq.com>
2019-05-22 11:44:55 +0800

    add vector.cu

---
+++ ml/src/math/vector.cu
@@ -0,0 +1,46 @@
+#include <math_functions.hpp>
+#include <assert.h>
+#include "common/include/type.h"
+#include "common/include/common.h"
+#include "ml/include/math/vector.h"
+
+# define TILE_HEIGHT 32
+# define TILE_WIDTH 32
+
+template<typename T> __global__ void
+vector_repeat_by_rows(T *mat_device, u32 rows_mat, u32 cols_mat, 
+              T *vector, u32 cols_vec){
+
+    // TODO: this function can be optimization
+    assert(cols_mat == cols_vec);
+
+    u32 idy = blockIdx.y*blockDim.y + threadIdx.y;
+    u32 idx = blockIdx.x*blockDim.x + threadIdx.x;
+
+    if(idx >= cols_mat || idy >= rows_mat)
+        return ;
+
+    mat_device[idy*cols_mat+idx] = vector[idx];
+
+}
+
+template<typename T> void
+vector_repeat_by_rows_launch(T *mat_device, u32 rows_mat, u32 cols_mat,
+              T *vector_device, u32 cols_vec){
+    
+    dim3 grid(MAX(1, (size_t)ceil((double)cols_mat/TILE_HEIGHT)),
+              MAX(1, (size_t)ceil((double)rows_mat/TILE_WIDTH)) );
+    dim3 block(TILE_WIDTH, TILE_HEIGHT);
+
+    vector_repeat_by_rows<T><<<grid, block>>>(mat_device, rows_mat, cols_mat,
+                        vector_device, cols_vec);
+}
+
+
+
+void
+vector_repeat_by_rows_cpu(float *mat_device, u32 rows_mat, u32 cols_mat,
+                          float *vector_device,u32 cols_vec){
+    vector_repeat_by_rows_launch(mat_device, rows_mat, cols_mat,
+                              vector_device, cols_vec);
+}
