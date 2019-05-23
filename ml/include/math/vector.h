ff06e6119cb54b27f3bac98ec9a6392d87624127
shouhuxianjian <342888106@qq.com>
2019-05-23 17:35:29 +0800

    add head content

--- ml/include/math/vector.h
+++ ml/include/math/vector.h
@@ -5,3 +5,6 @@ vector_repeat_by_rows_cpu(float *mat_device, u32 rows_mat, u32 cols_mat,
                           float *vector_device, u32 cols_vec);
 void
 vector_invsqrt_self_cpu(float *vec, u32 len);
+
+void
+vector_sqrt_self_cpu(float *vec, u32 len);
