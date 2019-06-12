f836f9a496a49c89db37f7a73b10a5f1c481d52c
shouhuxianjian <342888106@qq.com>
2019-06-12 19:20:25 +0800

    rename math

--- ml/include/math/math.h
+++ ml/include/math/math.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "ml/include/math/matrix.h"
-#include "ml/include/math/svd.h"
+#include "ml/include/math/matrix_solver.h"
 #include "ml/include/math/stats.h"
 #include "ml/include/math/vector.h"
 #include "ml/include/math/scalar_op_def.h"
